a9beb6cad7954dfcbd2e99802d7d9e946799c770
Williams Acosta <182219199+williams2609@users.noreply.github.com>
2024-11-22 20:32:36 +0100

    cambio de bcrypt a bcryptjs para compatibilidad

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const authMiddleware = require('../Middleware/authMiddleware');
